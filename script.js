5a08e9df6c65c01c89227a5b8d9506955a96bde9
Anthony muro <117108039+Anthonymuro@users.noreply.github.com>
2024-08-27 02:48:37 +0000

    labs

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// Write your JavaScipt code below this line. 
+// Write your JavaScipt code below this line.
+ const mainTitle = document.getElementById("main-title")
+ mainTitle.textContent = 'Welcome to Our Cat Adoption Center'
+
+ const description = document.getElementById("description")
+ description.textContent = 'Find your purrfect companion!'
+
+ const cat1button = document.getElementById("cat-1-button")
+ cat1button.addEventListener("click",() => {
+    console.log('clicked the first cat')
+    const cat =document.getElementById("cat-1")
+    alert("Congrats on adopting your new friend!")
+    cat.remove()
+ })
+
+ const cat2button = document.getElementById("cat-2-button")
+ cat2button.addEventListener("click",() => {
+    console.log('clicked the second cat')
+ })
+
+ const cat3button = document.getElementById("cat-3-button")
+ cat3button.addEventListener("click",() => {
+    console.log('clicked the third cat')
+ })
+
